548a6427ee101e442f8c04fbc1776751508444dc
YangZuhai <2840723492@qq.com>
2021-12-13 12:15:44 +0800

    删除了springplus

--- mybatisplus/src/main/java/com/entity/User.java
+++
@@ -1,6 +0,0 @@
-package com.entity;/**
- * 
- *@author lulei
- *@date 2021年12月10日 21:11:01
- */public class User {
-}
--- mybatisplus/src/main/java/com/hxzyyn/dao/UserDao.java
+++
@@ -1,6 +0,0 @@
-package com.hxzyyn.dao;/**
- * 
- *@author lulei
- *@date 2021年12月10日 21:14:30
- */public interface UserDao {
-}
